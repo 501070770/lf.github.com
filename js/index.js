cdaf603c4dec139f0add97bcf3cd4bd3207960b4
lifan <501070770@qq.com>
2017-04-25 17:16:22 +0800

    个人站

---
+++ js/index.js
@@ -0,0 +1,51 @@
+window.onload = function() {
+	//调用swiper插件
+	(function() {
+		var swiper = new Swiper('.swiper-container', {
+			pagination: '.swiper-pagination',
+			paginationClickable: true
+		});
+	})();
+	//添加导航
+	(function() {
+		var oNav = document.querySelector('#nav');
+		var aSpan = oNav.querySelectorAll('.swiper-pagination-bullet');
+		var arrNavP=['Home','WebKnow','MyCase','AboutMe'];
+		var arrNavD=['首页','前端知识','我的作品','关于本人'];
+		for(var i = 0; i < aSpan.length; i++) {
+			$(aSpan[i]).addClass('box');
+			aSpan[i].innerHTML = '<p>'+arrNavP[i]+'</p><div class="content">'+arrNavD[i]+'</div>';
+		}
+	})();
+	//我的案例，添加鼠标移入移出动画效果
+	(function() {
+		var aA = document.querySelectorAll('.con a');
+		var aShade = document.querySelectorAll('.shade');
+		for(var i = 0; i < aA.length; i++) {
+			(function(index) {
+				aA[i].onmouseenter = function() {
+					for(var j = 0; j < aShade.length; j++) {
+						aShade[j].className = 'shade';
+					}
+					aShade[index].className = 'shade shade1';
+				}
+				aA[i].onmouseleave = function() {
+					for(var j = 0; j < aShade.length; j++) {
+						aShade[j].className = 'shade';
+					}
+					aShade[index].className = 'shade shade2';
+				}
+			})(i);
+		}
+	})();
+	//为 我的案例 最后一个li添加高度
+	(function() {
+		var liH = document.querySelector('.case li').offsetHeight;
+		document.querySelector('.case li:last-child').style.height = liH + 'px';
+	})();
+	//为 前端开发 最后一个li添加高度
+	(function() {
+		var liH = document.querySelector('.web li').offsetHeight;
+		document.querySelector('.web .fr li:last-child').style.height = liH + 'px';
+	})();
+}
